a4ed4c42c71fa5890cea7d3116bf3c06cfc430b4
michelle-lo <mlo20@stuy.edu>
2020-11-10 02:23:47 -0500

    zip done and tested

--- Demo.java
+++ Demo.java
@@ -34,6 +34,21 @@ public class Demo {
       d.add("2"); d.add("1"); d.add("8");
       System.out.println(findOverlap(c, d).toString());
       //expected: [4. 2, 1]
+      SuperArray e = new SuperArray();
+      e.add("a"); e.add("b"); e.add("c"); e.add("d"); e.add("e"); e.add("f");
+      SuperArray f = new SuperArray();
+      f.add("0"); f.add("1"); f.add("2"); f.add("3");
+      System.out.println(zip(e, f).toString());
+      //Expected: ["a","0","b","1","c","2", "d","3","e","f"]
+
+      SuperArray g = new SuperArray();
+      g.add("a"); g.add("b"); g.add("c");
+      SuperArray h = new SuperArray();
+      h.add("0"); h.add("1"); h.add("2"); h.add("3"); h.add("4");
+      System.out.println(zip(g, h).toString());
+      //expected:["a","0","b","1","c","2","3","4"]
+      //[a, 0, b, 1, c, 2, 2, 3, 4]
+
 
 
 
@@ -43,8 +58,6 @@ public class Demo {
   You will be writing a method public static void removeDuplicates(SuperArray s){}
   When there are duplicates: all elements with the same value but higher index
   will be removed [you keep the first occurrence of each value].
-
-
   */
   public static void removeDuplicates(SuperArray s) {
     for (int i = 0; i < s.size(); i++) {
@@ -68,5 +81,36 @@ public class Demo {
       }
     }
     return c;
+  }
+
+
+  //takes two SuperArrays and merge them together.
+  //Return a new SuperArray that contains all elements of a,
+  //and all elements of b in the following sequence:
+  //[a0, b0, a1, b1, a2, b2 ... ]
+  //When either SuperArray is longer and you run out of strings in one,
+  //just fill the remaining elements with the rest of the longer SuperArray.
+  public static SuperArray zip(SuperArray a, SuperArray b){
+    SuperArray zipped = new SuperArray();
+    int last = 0;
+    for (int i = 0; i < a.size() && i < b.size(); i++) {
+      zipped.add(a.get(i));
+      zipped.add(b.get(i));
+      last = i;
+    }
+
+    if (a.size() == b.size()) {
+      return zipped;
+    } else if (a.size() > b.size()) {
+      for (int i = last + 1; i < a.size(); i++) {
+        zipped.add(a.get(i));
+      }
+    } else {
+      for (int i = last + 1; i < b.size(); i++) {
+        zipped.add(b.get(i));
+      }
+    }
+
+    return zipped;
   }
 }
